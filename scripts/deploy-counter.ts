2336a02fe84a6f826867cf14dff5a21e7ef4ac0d
Awtian <me@awtian.com>
2022-03-02 23:41:20 +0700

    pre frontend for count

---
+++ scripts/deploy-counter.ts
@@ -0,0 +1,19 @@
+import { isCommunityResourcable } from "@ethersproject/providers";
+import "@nomiclabs/hardhat-ethers";
+import {ethers} from "hardhat";
+
+async function deploy () {
+  const Counter = await ethers.getContractFactory("Counter");
+  const counter = await Counter.deploy();
+  await counter.deployed();
+
+  return counter;
+}
+
+
+async function count(counter) {
+  await counter.count()
+  console.log("Counter", await counter.getCounter());
+}
+
+deploy().then(count);
